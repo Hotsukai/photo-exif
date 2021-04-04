f6d7fca05d9f08c293a54b8531740e760b76254c
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2021-04-04 14:37:18 +0900

    update exif情報をコンソールに表示

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="file" @change="imageInputed" />
+    <img :src="inputedImageSrc" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import EXIF from "exif-js";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      inputedFile: {},
+      inputedImageSrc: "",
+    };
+  },
+  methods: {
+    imageInputed(event) {
+      this.inputedFile = event.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(this.inputedFile);
+      reader.onload = () => {
+        this.inputedImageSrc = reader.result;
+      };
+      EXIF.getData(this.inputedFile, function () {
+        console.debug("debug2 :", this);
+        console.debug(EXIF.getAllTags(this));
+      });
+    },
+  },
+};
 </script>
 
 <style>
